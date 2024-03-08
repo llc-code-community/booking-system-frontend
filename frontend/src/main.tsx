b1fd2d996570f4cb2c727fc24ac5d4ca750e975a
Meow-Double <slava.pugachev.2015@mail.ru>
2024-03-08 22:46:15 +0300

    fix: initial project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import './static/styles/global.css';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
