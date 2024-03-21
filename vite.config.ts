6efc0651bddb4d9991f47ad669a8661f01880a4a
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-21 22:48:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,26 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// const jsToBottomNoModule = () => {
-//   return {
-//     name: 'no-attribute',
-//     transformIndexHtml(html) {
-//       html = html.replace(`type="module" crossorigin`, '');
-//       let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0];
-//       console.log('\n SCRIPT TAG', scriptTag, '\n');
-//       html = html.replace(scriptTag, '');
-//       html = html.replace('<!-- # JS SCRIPT -->', scriptTag);
-//       return html;
-//     },
-//   };
-// };
+const jsToBottomNoModule = () => {
+  return {
+    name: 'no-attribute',
+    transformIndexHtml(html) {
+      html = html.replace(`type="module" crossorigin`, '');
+      let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0];
+      console.log('\n SCRIPT TAG', scriptTag, '\n');
+      html = html.replace(scriptTag, '');
+      html = html.replace('<!-- # JS SCRIPT -->', scriptTag);
+      return html;
+    },
+  };
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), jsToBottomNoModule()],
   resolve: {
     alias: {
       src: '/src',
