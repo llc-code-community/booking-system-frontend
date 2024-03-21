efdd1ada0b0cbadde5e4f35acc6a07f3c55314ed
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-21 22:53:04 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ const plugins = [];
 if (process.env.NODE_ENV === 'development') {
   plugins.push('react-refresh/babel');
 }
+plugins.push('babel-plugin-transform-import-meta');
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'], // пресет для работы с React
