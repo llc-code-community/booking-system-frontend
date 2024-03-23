92573e6ab60a5562334eb0874d964c73c74500a6
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-23 20:37:50 +0100

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,24 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-  },
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      src: '/src',
-      components: '/src/components',
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ command }) => {
+  const config = {
+    server: {
+      port: 3000,
+      open: true,
     },
-  },
-  base: '/',
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        src: '/src',
+        components: '/src/components',
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+    base: '/',
+  };
+  if (command !== 'serve') {
+    config.base = '/booking-system-frontend/';
+  }
+  return config;
 });
