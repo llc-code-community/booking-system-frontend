da5108cb921a4ee68191e9808c6e4921f889b530
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-23 10:18:38 +0100

    Update template

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,13 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const jsToBottomNoModule = () => {
-  return {
-    name: 'no-attribute',
-    transformIndexHtml(html) {
-      html = html.replace(`type="module" crossorigin`, '');
-      let scriptTag = html.match(/<script[^>]*>(.*?)<\/script[^>]*>/)[0];
-      console.log('\n SCRIPT TAG', scriptTag, '\n');
-      html = html.replace(scriptTag, '');
-      html = html.replace('<!-- # JS SCRIPT -->', scriptTag);
-      return html;
-    },
-  };
-};
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
     open: true,
   },
-  plugins: [react(), tsconfigPaths(), jsToBottomNoModule()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       src: '/src',
