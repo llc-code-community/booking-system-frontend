9156f0be823e7efaaf9c83acf1caba2f8dcb34a8
AndreiGutnik <andreigutnik@gmail.com>
2024-03-24 09:14:42 +0000

    Deploying to gh-pages from @ llc-code-community/booking-system-frontend@5b091f078cd03ae9e3908c9041166cc29202a1be 🚀

---
+++ assets/MainPage-cgbmcp4F.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-UV6NDX7S.js";function e({}){return n.jsx("main",{children:n.jsx("h1",{children:"Main page"})})}export{e as default};
--- assets/MainPage-inBa6XMb.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-WWfkdiZc.js";function e({}){return n.jsx("main",{children:n.jsx("h1",{children:"Main page"})})}export{e as default};
