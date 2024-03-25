117e530c2a0b881a568c3f958098f8dd9c03d582
Nazarii <dzhaman.nazar2003@gmail.com>
2024-03-25 11:06:10 +0200

    add markup

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,72 @@
 export const Footer = () => {
-  return (
-    <footer>
-      <p>Footer</p>
-    </footer>
-  );
+  return <footer>
+  <div>
+      <p>Logo</p>
+      <p>@2024 Logo</p>
+  </div>
+  <div>
+      <p>Connect</p>
+      <ul>
+          <li>
+              <a href="/">Instagram</a>
+          </li>
+          <li>
+              <a href="/">Linkedln</a>
+          </li>
+          <li>
+              <a href="/">Facebook</a>
+          </li>
+      </ul>
+  </div>
+  <div>
+      <p>Information</p>
+      <ul>
+          <li>
+              <a href="/">How it works</a>
+          </li>
+          <li>
+              <a href="/">Support</a>
+          </li>
+      </ul>
+  </div>
+</footer>
 };
+
+
+// import css from './Footer.module.css'
+
+// const Footer = () => {
+//     return <footer className={css.footer}>
+//         <div>
+//             <p className={css.footer_logo}>Logo</p>
+//             <p className={css.footer_year}>@2024 Logo</p>
+//         </div>
+//         <div className={css.connect}>
+//             <p className={css.connect_name}>Connect</p>
+//             <ul className={css.connect_list}>
+//                 <li>
+//                     <a href="/">Instagram</a>
+//                 </li>
+//                 <li>
+//                     <a href="/">Linkedln</a>
+//                 </li>
+//                 <li>
+//                     <a href="/">Facebook</a>
+//                 </li>
+//             </ul>
+//         </div>
+//         <div className={css.information}>
+//             <p className={css.information_name}>Information</p>
+//             <ul className={css.information_list}>
+//                 <li>
+//                     <a href="/">How it works</a>
+//                 </li>
+//                 <li>
+//                     <a href="/">Support</a>
+//                 </li>
+//             </ul>
+//         </div>
+//     </footer>
+// }
+
+// export default Footer
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,43 @@
 export const Header = () => {
-  return (
-    <header>
-      <p>Header</p>
-    </header>
-  );
+  return <header>
+  <div>
+      <p>Logo</p>
+      <p>Beta</p>
+  </div>
+  <nav>
+      <p>List your property</p>
+      <div>
+          <p>Favorites</p>
+      </div>
+      <div>
+          <p>P</p>
+      </div>
+  </nav>
+</header>
 };
+
+// import css from './Header.module.css'
+// import FavoriteIcon from "../../assets/favorite-icon.svg?react";
+// import DownIcon from '../../assets/down-icon.svg?react'
+
+// const Header = () => {
+//     return <header className={css.header}>
+//         <div className={css.logo}>
+//             <p className={css.logo_text}>Logo</p>
+//             <p className={css.logo_beta}>Beta</p>
+//         </div>
+//         <nav className={css.navbar}>
+//                 <p className={css.navbar_list}>List your property</p>
+//                 <div className={css.navbar_favorites}>
+//                     <p>Favorites</p>
+//                     <FavoriteIcon/>
+//                 </div>
+//                 <div className={css.navbar_name}>
+//                     <p className={css.navbar_literal}>P</p>
+//                     <DownIcon/>
+//                 </div>
+//         </nav>
+//     </header>
+// }
+
+// export default Header
